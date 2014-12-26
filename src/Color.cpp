11a47acd06b0c6a42b58ec0c54c597da6fc51038
David Barnett <david@barnett.net.nz>
2014-12-27 11:42:25 +1300

    More Color constructors
    + more constructors and tests for Color

--- src/Color.cpp
+++ src/Color.cpp
@@ -1,5 +1,5 @@
 #include "Color.h"
-
+#include <limits>
 using namespace tiledl;
 
 Color::Color(SDL_Color& c)
@@ -8,4 +8,86 @@ Color::Color(SDL_Color& c)
 	this->g = c.g;
 	this->b = c.b;
 	this->a = c.a;
+}
+
+Color::Color(Uint8 r, Uint8 g, Uint8 b, Uint8 a)
+{
+	this->r = r;
+	this->g = g;
+	this->b = b;
+	this->a = a;
+}
+
+Color::Color(Uint8 r, Uint8 g, Uint8 b)
+{
+	this->r = r;
+	this->g = g;
+	this->b = b;
+	this->a = 255;
+}
+
+Color::Color(int r, int g, int b, int a)
+{
+	this->r = (Uint8)(r);
+	this->g = (Uint8)(g);
+	this->b = (Uint8)(b);
+	this->a = (Uint8)(a);
+}
+
+Color::Color(int r, int g, int b)
+{
+	this->r = (Uint8)(r % 256);
+	this->g = (Uint8)(g % 256);
+	this->b = (Uint8)(b % 256);
+	this->a = 255;
+}
+
+/**
+ * @brief Follows HTML-styled hex notation of byte order of RGBA
+ *
+ * @param rgba an int containing a RGBA8888 color
+ */
+Color::Color(int rgba)
+{
+	this->r = (Uint8)((rgba >> 24) % 256);
+	this->g = (Uint8)((rgba >> 16) % 256);
+	this->b = (Uint8)((rgba >>  8) % 256);
+	this->a = (Uint8)((rgba >>  0) % 256);
+}
+
+/**
+ * @brief Load Color from int using byte desired ordering
+ *
+ * @param rgba an int containing a RGBA8888 color
+ * @param order Byte order to use
+ */
+Color::Color(int rgba, ColorByteOrder order)
+{
+	switch (order) {
+		case(LITTLE_ENDIAN_ORDER):
+			this->r = (Uint8)((rgba >> 24) % 256);
+			this->g = (Uint8)((rgba >> 16) % 256);
+			this->b = (Uint8)((rgba >>  8) % 256);
+			this->a = (Uint8)((rgba >>  0) % 256);
+			break;
+		case(BIG_ENDIAN_ORDER):
+			this->r = (Uint8)((rgba >>  0) % 256);
+			this->g = (Uint8)((rgba >>  8) % 256);
+			this->b = (Uint8)((rgba >> 16) % 256);
+			this->a = (Uint8)((rgba >> 24) % 256);
+			break;
+		case(SYSTEM_ORDER):
+#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+			this->r = (Uint8)((rgba >>  0) % 256);
+			this->g = (Uint8)((rgba >>  8) % 256);
+			this->b = (Uint8)((rgba >> 16) % 256);
+			this->a = (Uint8)((rgba >> 24) % 256);
+#else
+			this->r = (Uint8)((rgba >> 24) % 256);
+			this->g = (Uint8)((rgba >> 16) % 256);
+			this->b = (Uint8)((rgba >>  8) % 256);
+			this->a = (Uint8)((rgba >>  0) % 256);
+#endif
+			break;
+	}
 }
--- src/Color.h
+++ src/Color.h
@@ -6,9 +6,21 @@
 
 namespace tiledl
 {
+	enum ColorByteOrder {
+		LITTLE_ENDIAN_ORDER,
+		BIG_ENDIAN_ORDER,
+		SYSTEM_ORDER
+	};
+
 	struct Color : public SDL_Color {
 	public:
 		Color(SDL_Color& c);
+		Color(Uint8 r, Uint8 gr, Uint8 b, Uint8 a);
+		Color(Uint8 r, Uint8 gr, Uint8 b);
+		Color(int r, int gr, int b, int a);
+		Color(int r, int gr, int b);
+		Color(int rgba);
+		Color(int rgba, ColorByteOrder order);
 	};
 }
 
